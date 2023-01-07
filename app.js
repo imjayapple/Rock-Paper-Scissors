3eb2f8f011137ff57b9b80b24c53eb9b9ebc58fc
imjayapple <thisisfakeemail@gmai.com>
2023-01-06 22:16:26 -0800

    Initial commit, add the function to capture user input and save input as lowercase

---
+++ app.js
@@ -0,0 +1,6 @@
+//Create a function that captures the users input and saves it to lowercase so that ex. "rock" = "ROCK"
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+
+    
+}

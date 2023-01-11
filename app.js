94dba7784fb9aad1b84e44d688739204ea2fb881
imjayapple <imjayapple@gmail.com>
2023-01-10 22:54:26 -0800

    Function to generate the computers' choice for RPS game

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//Create a function that captures the users input and saves it to lowercase so that ex. "rock" = "ROCK"
-//Add an if/else to accept the user input
+//Create a function that captures the users choice and saves it to a lowercase value so that ex. "RocK" = "rock"
+//Add an if/else to process the user input
 const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
     if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
@@ -11,3 +11,21 @@ const getUserChoice = userInput => {
 }
 
 //Test function by calling it
+// let myTest = getUserChoice("ROck");
+// let myTest2 = getUserChoice("ahh");
+// console.log(myTest);
+// console.log(myTest2);
+
+//A function to generate the computer's choice, rock, paper, or scissors using switch statement and three random
+//numerical values between 0-2 (0, 1, 2)
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+};

cb0d3ca27508e82bfad3e407a72e32767a8db58e
imjayapple <imjayapple@gmail.com>
2023-01-10 22:41:05 -0800

    If else to accept a valid input, return said input, or log an error for invalid input

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 //Create a function that captures the users input and saves it to lowercase so that ex. "rock" = "ROCK"
+//Add an if/else to accept the user input
 const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
-
-    
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+        return userInput;
+      }
+      else {
+        console.log('Error, invalid input');
+      }
 }
+
+//Test function by calling it
